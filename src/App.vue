7de670df50eecb081f96acbc998a94ef34b2f3a6
Kabir Khyrul <kabir.signup@gmail.com>
2025-07-19 22:21:44 +0600

    feat: Add header component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
 <template>
-    <h1 class="text-5xl">Hello</h1>
+    <Header />
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="mb-6">
+        <h1 class="text-xl font-bold text-gray-900 mb-1">
+            YouTube Queue Builder
+        </h1>
+        <p class="text-sm text-gray-600">
+            Scan and queue all videos from search results
+        </p>
+    </div>
+</template>
